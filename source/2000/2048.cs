303455b764d26e1302280b7d0f6dab2f8b8352b0
nayun <han.nayun@icloud.com>
2023-12-09 11:18:36 +0800

    Add Solution and Test for NextBeautifulNumber
    Implemented a new class named Solution in the namespace source._2000._2048 with the method NextBeautifulNumber to find the next "beautiful" number. Added corresponding tests in Test2048 under the namespace test._2000 to verify the solution. The test case was then registered in DotSettings.user file.

---
+++ source/2000/2048.cs
@@ -0,0 +1,28 @@
+namespace source._2000._2048;
+
+public class Solution
+{
+    
+    public int NextBeautifulNumber(int n)
+    {
+        for (var i = n + 1; i < 10e6; ++i)
+            if (IsBeautiful(i))
+                return i;
+
+        return -1;
+    }
+
+    private static bool IsBeautiful(int n)
+    {
+        var count = new Dictionary<int, int>();
+        while (n > 0)
+        {
+            var digit = n % 10;
+            count.TryAdd(digit, 0);
+            ++count[digit];
+            n /= 10;
+        }
+
+        return count.All(kv => kv.Key == kv.Value);
+    }
+}
---
+++ test/2000/2048.cs
@@ -0,0 +1,28 @@
+using source._2000._2048;
+
+namespace test._2000;
+
+[TestClass]
+public class Test2048
+{
+    [TestMethod]
+    [Timeout(1000)]
+    public void NormalCase()
+    {
+        var solution = new Solution();
+        var n = 1;
+        var expected = 22;
+        var actual = solution.NextBeautifulNumber(n);
+        Assert.AreEqual(expected, actual);
+
+        n = 1000;
+        expected = 1333;
+        actual = solution.NextBeautifulNumber(n);
+        Assert.AreEqual(expected, actual);
+
+        n = 3000;
+        expected = 3133;
+        actual = solution.NextBeautifulNumber(n);
+        Assert.AreEqual(expected, actual);
+    }
+}
